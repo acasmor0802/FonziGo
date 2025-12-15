32942d8ee12e79757bc676d59a17ee16c57a7470
acasmor0802 <cmalfonso@iesrafaelalberti.es>
2025-12-15 10:54:41 +0100

    tests backend

--- backend/src/test/java/fonzigo/backend/controller/AuthenticationControllerTest.java
+++ backend/src/test/java/fonzigo/backend/controller/AuthenticationControllerTest.java
@@ -5,10 +5,12 @@ import fonzigo.backend.dto.UsuarioRegistroDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthenticationControllerTest {
 
     @Autowired
--- backend/src/test/java/fonzigo/backend/controller/ProductControllerTest.java
+++ backend/src/test/java/fonzigo/backend/controller/ProductControllerTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductControllerTest {
 
     @Autowired
